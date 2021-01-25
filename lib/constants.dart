015e71a207d58ee86e4e9c5c5cce223f07973a34
vnl <vivian@vivianlobo.com>
2021-01-25 09:50:27 +0000

    new xml feed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,3 @@
-const kPodcastFeed = '';
+const kPodcastFeed =
+    'https://raw.githubusercontent.com/vnl/podcast_player/master/feed.xml';
 const kSpotifyLink = 'https://open.spotify.com/show/58qvAMBmC4A4fMsd9Wgvna';
